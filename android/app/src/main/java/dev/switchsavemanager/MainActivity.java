b750fb1bbc994c4a8335b1df547d6e0dc3f4994c
diazleonata <diazleonata@outlook.com>
2025-08-23 21:17:06 +0700

    initial

---
+++ android/app/src/main/java/dev/switchsavemanager/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.switchsavemanager;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.switchsavemanager',
+  appName: 'SwitchSaveManager',
+  webDir: 'dist'
+};
+
+export default config;
